1d791f7b550d4bd5d7784e5cd9b8dff04f78bc36
Anna <anna.voloshchuk@gmail.com>
2014-07-25 01:58:49 +0300

    3 Actions to fill in DB tables

---
+++ MyMvcApplicationTest/MyMvcApplicationTest/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using MyMvcApplicationTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            //var entity = new MyBestDBEntities();
+            //entity.Locations.Add(new Location { Name = "Kyiv" });
+
+            //var locations = entity.Locations.ToList().First(x => x.Id == 1);
+
+            ////entity.Producers.Add(new Producer
+            ////{
+            ////    Location = locations,
+            ////    Name = "Saturn"
+            ////});
+
+            //var prod = entity.Producers.ToList().First(x => x.Id == 1);
+
+            //entity.Products.Add(new Product
+            //{
+            //    Producer = prod,
+            //    Name = "TV",
+            //    Count = 1,
+            //    Price = 10.1m
+
+            //});
+     
+            //entity.SaveChanges();
+            return View();
+        }
+
+        public ActionResult AddLocation()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddLocation(FormCollection form)
+        {
+            var entity = new MyBestDBEntities();
+
+            var location = new Location{ Name = form["Name"] };
+            entity.Locations.Add(location);
+            entity.SaveChanges();
+            return View();
+        }
+
+        public ActionResult AddProducer()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult AddProducer(FormCollection form)
+        {
+            var entity = new MyBestDBEntities();
+
+            var producer = new Producer { Name = form["Name"], LocationId = Convert.ToInt32(form["LocationId"])};
+            entity.Producers.Add(producer);
+            entity.SaveChanges();
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(FormCollection form)
+        {
+            var entity = new MyBestDBEntities();
+            var product = new Product { Name = form["Name"], Price = Convert.ToDecimal(form["Price"]), Count = Convert.ToInt32(form["Count"])};
+            entity.Products.Add(product);
+            entity.SaveChanges();
+            return View();
+        }
+
+    }
+}
---
+++ MyMvcApplicationTest/MyMvcApplicationTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMvcApplicationTest.MvcApplication" Language="C#" %>
