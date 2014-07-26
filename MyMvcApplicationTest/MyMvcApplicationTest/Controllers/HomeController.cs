2bf0c638e7e2dc8f5c30584653d34cceb0b83c61
Anna <anna.voloshchuk@gmail.com>
2014-07-27 02:30:53 +0300

    DB fill in

--- MyMvcApplicationTest/MyMvcApplicationTest/Controllers/HomeController.cs
+++ MyMvcApplicationTest/MyMvcApplicationTest/Controllers/HomeController.cs
@@ -38,53 +38,114 @@ namespace MyMvcApplicationTest.Controllers
             return View();
         }
 
+       // public ActionResult AddLocation()
+       // {
+            
+       //     return View();
+       // }
+
+       // [HttpPost]
+       // public ActionResult AddLocation(FormCollection form)
+       // {
+       //     var entity = new MyBestDBEntities();
+
+       //     var location = new Location{ Name = form["Name"] };
+       //     entity.Locations.Add(location);
+       //     entity.SaveChanges();
+       //     return View();
+       // }
+
+       // public ActionResult AddProducer()
+       // {
+       //     return View();
+       // }
+
+       //[HttpPost]
+       // public ActionResult AddProducer(FormCollection form)
+       // {
+       //     var entity = new MyBestDBEntities();
+
+       //     var producer = new Producer { Name = form["Name"], LocationId = Convert.ToInt32(form["LocationId"])};
+       //     entity.Producers.Add(producer);
+       //     entity.SaveChanges();
+       //     return View();
+       // }
+
+       // public ActionResult AddProduct()
+       // {
+       //     return View();
+       // }
+
+       // [HttpPost]
+       // public ActionResult AddProduct(FormCollection form)
+       // {
+       //     var entity = new MyBestDBEntities();
+       //     var product = new Product { Name = form["Name"], Price = Convert.ToDecimal(form["Price"]), Count = Convert.ToInt32(form["Count"])};
+       //     entity.Products.Add(product);
+       //     entity.SaveChanges();
+       //     return View();
+       // }
+
+        
+
         public ActionResult AddLocation()
         {
-            
-            return View();
+            return View(new Location ());
         }
 
         [HttpPost]
-        public ActionResult AddLocation(FormCollection form)
+        public ActionResult SaveLocation(Location model)
         {
             var entity = new MyBestDBEntities();
-
-            var location = new Location{ Name = form["Name"] };
-            entity.Locations.Add(location);
+            entity.Locations.Add(model);
             entity.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
         }
 
         public ActionResult AddProducer()
         {
-            return View();
+            var entity = new MyBestDBEntities();
+            var locations = entity.Locations.ToList();
+
+            var locationList = locations.Select(x => { return new SelectListItem { Text = x.Name, Value = x.Id.ToString() }; }).ToList();
+
+            ViewBag.Locations = locationList;
+            return View(new Producer());
         }
 
-       [HttpPost]
-        public ActionResult AddProducer(FormCollection form)
+        public ActionResult SaveProducer(Producer model)
         {
             var entity = new MyBestDBEntities();
-
-            var producer = new Producer { Name = form["Name"], LocationId = Convert.ToInt32(form["LocationId"])};
-            entity.Producers.Add(producer);
+            var location = entity.Locations.ToList().First(x => x.Id == model.LocationId);
+            model.Location = location;
+            entity.Producers.Add(model);
             entity.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult AddProduct()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AddProduct(FormCollection form)
+       public ActionResult AddProduct()
         {
             var entity = new MyBestDBEntities();
-            var product = new Product { Name = form["Name"], Price = Convert.ToDecimal(form["Price"]), Count = Convert.ToInt32(form["Count"])};
-            entity.Products.Add(product);
-            entity.SaveChanges();
-            return View();
+            var producers = entity.Producers.ToList();
+            var producerList = producers.Select(x => { return new SelectListItem { Text = x.Name, Value = x.Id.ToString() }; }).ToList();
+
+            ViewBag.Producers = producerList;
+           
+            return View(new Product());
         }
+
+         [HttpPost]
+       public ActionResult SaveProduct(Product model)
+       {
+           var entity = new MyBestDBEntities();
+           var producer = entity.Producers.ToList().First(x => x.Id == model.ProducerID);
+           model.Producer = producer;
+
+           entity.Products.Add(model);
+           entity.SaveChanges();
+
+           return RedirectToAction("Index");
+       }
 
     }
 }
